1a38a8378bece3c166f69a0db6b6085dc6e6ad37
Erico-Muller <ericomuller0@gmail.com>
2022-08-01 15:45:33 -0300

    web: initial commit

---
+++ packages/web/pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+   return (
+      <>
+         <Head>
+            <title>Hello World!</title>
+         </Head>
+
+         <h1>Hello World!</h1>
+      </>
+   )
+}
+
+export default Home
