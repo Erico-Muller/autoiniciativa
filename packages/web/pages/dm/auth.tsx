366c66374e718adc04bfacd0c8c82b5765a14be2
Erico-Muller <ericomuller0@gmail.com>
2022-08-04 17:52:37 -0300

    web: dm auth page added

---
+++ packages/web/pages/dm/auth.tsx
@@ -0,0 +1,89 @@
+import { useRef, FormEvent } from 'react'
+
+import Head from 'next/head'
+import Link from 'next/link'
+import type { NextPage } from 'next'
+
+import { toast, ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
+const DMAuth: NextPage = () => {
+   const inputPassRef = useRef<HTMLInputElement>(null)
+
+   function handleSubmit(event: FormEvent) {
+      event.preventDefault()
+
+      if (!inputPassRef.current?.value)
+         return emitError('Insira a senha do mestre')
+
+      console.log(inputPassRef.current.value)
+   }
+
+   function emitError(message: string) {
+      toast.error(message, {
+         position: 'bottom-right',
+         autoClose: 2000,
+         hideProgressBar: true,
+         closeOnClick: true,
+         pauseOnHover: true,
+         draggable: true
+      })
+   }
+
+   return (
+      <>
+         <Head>
+            <title>Enter</title>
+         </Head>
+
+         <div className="flex flex-col justify-center items-center min-h-screen py-8">
+            <header>
+               <h1 className="text-5xl text-center">
+                  Mestre
+               </h1>
+            </header>
+
+            <form onSubmit={handleSubmit} className="w-full h-48 sm:max-w-min sm:max-h-min m-8 p-8 flex flex-col justify-center items-center gap-8 bg-slate-700 sm:rounded-xl">
+               <input
+                  className="p-1 text-lg text-center text-gray-800 bg-gray-200 rounded-xl hover:scale-110 focus:outline-none focus:ring-4 focus:ring-violet-500 focus:ring-opacity-50 transition ease-in-out duration-200"
+                  type="password"
+                  placeholder="Senha"
+                  ref={inputPassRef}
+                  maxLength={15}
+                  minLength={1}
+               />
+
+               <button type="submit" className="px-2 py-1 border-2 border-gray-200 rounded-lg hover:text-slate-700 hover:bg-gray-200 transition ease-linear duration-150">
+                  Entrar
+               </button>
+            </form>
+
+            <Link href="/enter">
+               <a className="px-2 py-1 text-gray-800 bg-gray-200 border-2 border-gray-200 rounded-lg hover:text-gray-200 hover:bg-transparent transition ease-linear duration-150">
+                  Jogador
+               </a>
+            </Link>
+
+            <footer className="w-full pt-3 pb-6 fixed bottom-0 text-center bg-gray-800">
+               <p className="text-xs">
+                  Created by Erikinho
+               </p>
+            </footer>
+         </div>
+
+         <ToastContainer
+            position='bottom-right'
+            autoClose={2000}
+            hideProgressBar
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+         />
+      </>
+   )
+}
+
+export default DMAuth
